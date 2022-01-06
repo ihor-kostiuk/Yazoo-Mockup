9a36411ff43a0b6aee1706e60d953d1929b51dd3
Ihor Kostiuk <79281609+ihor-kostiuk@users.noreply.github.com>
2022-01-06 19:15:09 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+function imgSlider(anything)
+{
+  document.querySelector(".yazoo").src = anything;
+}
+
+function changeCircleDarkColor(color)
+{
+  const circle = document.querySelector(".circleDark");
+  circle.style.background = color;
+}
